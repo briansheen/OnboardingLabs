01b9026deb968065df61234c06144a1bef650fcd
Brian Sheen <brian.sheen@lt5904.local>
2017-12-19 16:53:05 -0600

    revised pom slf4j version, LOGGER changed to logger, and updated imports

--- src/main/java/com/company/GetTimeline.java
+++ src/main/java/com/company/GetTimeline.java
@@ -2,8 +2,14 @@ package com.company;
 
 import com.company.api.TimelineResponse;
 import com.company.api.TwitterErrorResponse;
-import twitter4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.ResponseList;
 
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -11,7 +17,7 @@ import java.util.List;
 
 public class GetTimeline {
 
-    private static Logger LOGGER = Logger.getLogger(GetTimeline.class);
+    private static Logger logger = LoggerFactory.getLogger(GetTimeline.class);
 
     public Response run(TwitterAppConfigurationKeys keys) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -30,7 +36,7 @@ public class GetTimeline {
             }
             return Response.ok(timelineResponses).build();
         } catch (TwitterException e) {
-            LOGGER.error("There was an error interacting with the Twitter API and/or Twitter Keys", e);
+            logger.error("There was an error interacting with the Twitter API and/or Twitter Keys", e);
             return Response.status(Response.Status.NOT_FOUND).entity(new TwitterErrorResponse(Response.Status.NOT_FOUND.getStatusCode(), "There was an error when trying to get your twitter timeline.")).build();
         }
     }
