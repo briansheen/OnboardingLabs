b042ac1317d7439169d7c970cbe83caf33a700a4
Brian Sheen <brian.sheen@lt5904.local>
2017-12-19 16:10:08 -0600

    added logging to tweet and gettimeline

--- src/main/java/com/company/GetTimeline.java
+++ src/main/java/com/company/GetTimeline.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetTimeline {
+
+    private static Logger LOGGER = Logger.getLogger(GetTimeline.class);
+
     public Response run(TwitterAppConfigurationKeys keys) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
@@ -19,15 +22,16 @@ public class GetTimeline {
                 .setOAuthAccessTokenSecret(keys.getoAuthAccessTokenSecret());
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
-        try{
+        try {
             ResponseList<Status> homeTimeline = twitter.getHomeTimeline();
             List<TimelineResponse> timelineResponses = new ArrayList<>();
-            for(Status s : homeTimeline){
-                timelineResponses.add(new TimelineResponse(s.getUser().getScreenName(),s.getText()));
+            for (Status s : homeTimeline) {
+                timelineResponses.add(new TimelineResponse(s.getUser().getScreenName(), s.getText()));
             }
             return Response.ok(timelineResponses).build();
-        } catch(TwitterException e){
-            return Response.status(Response.Status.NOT_FOUND).entity(new TwitterErrorResponse(Response.Status.NOT_FOUND.getStatusCode(),"There was an error when trying to get your twitter timeline.")).build();
+        } catch (TwitterException e) {
+            LOGGER.error("There was an error interacting with the Twitter API and/or Twitter Keys", e);
+            return Response.status(Response.Status.NOT_FOUND).entity(new TwitterErrorResponse(Response.Status.NOT_FOUND.getStatusCode(), "There was an error when trying to get your twitter timeline.")).build();
         }
     }
 }
--- src/main/java/com/company/TwitterAppConfiguration.java
+++ src/main/java/com/company/TwitterAppConfiguration.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class TwitterAppConfiguration extends Configuration {
 
     @Valid
+    @NotNull
     private TwitterAppConfigurationKeys twitterKeys;
 
     @JsonProperty
