d53aac28384158a8e6b596b9c373cc148b842a5e
Brian Sheen <brian.sheen@lt5904.local>
2018-01-09 09:40:51 -0600

    refactored DeckList prototype pattern

--- src/main/java/com/company/patterns/DeckList.java
+++ src/main/java/com/company/patterns/DeckList.java
@@ -5,26 +5,23 @@ import java.util.Map;
 
 public class DeckList implements Cloneable {
 
-    private Map<String, Map<String, Integer>> deck;
     private Map<String, Integer> creatureCards;
     private Map<String, Integer> planeswalkerCards;
     private Map<String, Integer> spellCards;
     private Map<String, Integer> landCards;
 
     public DeckList() {
-        deck = new HashMap<>();
         creatureCards = new HashMap<>();
         planeswalkerCards = new HashMap<>();
         spellCards = new HashMap<>();
         landCards = new HashMap<>();
     }
 
-    public DeckList(Map<String, Map<String, Integer>> deck) {
-        this.deck = deck;
-    }
-
-    public Map<String, Map<String, Integer>> getDeck() {
-        return deck;
+    public DeckList(Map<String, Integer> creatures, Map<String, Integer> planeswalkers, Map<String, Integer> spells, Map<String, Integer> lands) {
+        this.creatureCards = creatures;
+        this.planeswalkerCards = planeswalkers;
+        this.spellCards = spells;
+        this.landCards = lands;
     }
 
     public Map<String, Integer> getCreatureCards() {
@@ -50,15 +47,12 @@ public class DeckList implements Cloneable {
         creatureCards.put("Whirler Virtuoso", 4);
         creatureCards.put("Bristling Hydra", 3);
         creatureCards.put("The Scarab God", 3);
-        deck.put("Creatures", creatureCards);
         planeswalkerCards.put("Chandra, Torch of Defiance", 3);
         planeswalkerCards.put("Nicol Bolas, God-Pharaoh", 1);
-        deck.put("Planeswalkers", planeswalkerCards);
         spellCards.put("Attune with Aether", 4);
         spellCards.put("Abrade", 2);
         spellCards.put("Harnessed Lightning", 4);
         spellCards.put("Confiscation Coup", 1);
-        deck.put("Spells", spellCards);
         landCards.put("Aether Hub", 4);
         landCards.put("Botanical Sanctum", 4);
         landCards.put("Forest", 3);
@@ -68,26 +62,32 @@ public class DeckList implements Cloneable {
         landCards.put("Sheltered Thicket", 2);
         landCards.put("Spirebluff Canal", 4);
         landCards.put("Swamp", 1);
-        deck.put("Lands", landCards);
     }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        Map<String, Map<String, Integer>> temp = new HashMap<>();
-        for (String s : this.getDeck().keySet()) {
-            temp.put(s, this.getDeck().get(s));
+        Map<String, Integer> tempCreatures = cloneHelper(this.getCreatureCards());
+        Map<String, Integer> tempPlaneswalkers = cloneHelper(this.getPlaneswalkerCards());
+        Map<String, Integer> tempSpells = cloneHelper(this.getSpellCards());
+        Map<String, Integer> tempLands = cloneHelper(this.getLandCards());
+        return new DeckList(tempCreatures, tempPlaneswalkers, tempSpells, tempLands);
+    }
+
+    private Map<String, Integer> cloneHelper(Map<String, Integer> cardMap){
+        Map<String, Integer> newCardMap = new HashMap<>();
+        for(String cardName : cardMap.keySet()){
+            newCardMap.put(cardName, cardMap.get(cardName));
         }
-        return new DeckList(temp);
+        return newCardMap;
     }
 
     @Override
     public String toString() {
-        return "DeckList{" +
-                "deck=" + deck +
-                ", creatureCards=" + creatureCards +
-                ", planeswalkerCards=" + planeswalkerCards +
-                ", spellCards=" + spellCards +
-                ", landCards=" + landCards +
+        return "DeckList{\n" +
+                "creatureCards=" + creatureCards + "\n" +
+                "planeswalkerCards=" + planeswalkerCards + "\n" +
+                "spellCards=" + spellCards + "\n" +
+                "landCards=" + landCards + "\n" +
                 '}';
     }
 }
